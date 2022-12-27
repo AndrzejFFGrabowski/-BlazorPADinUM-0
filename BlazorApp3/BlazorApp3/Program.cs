be5b14b9df04aa5f9aa5574336f92d925772e40b
AndrzejFFGrabowski <Andrzej.f.f.Grabowski@gmail.com>
2022-12-27 18:35:09 +0100

    stupidCommit

--- BlazorApp3/BlazorApp3/Program.cs
+++ BlazorApp3/BlazorApp3/Program.cs
@@ -2,10 +2,6 @@ using BlazorApp3;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+var builder = Startup.InitializeApp(args);
 await builder.Build().RunAsync();
---
+++ BlazorApp3/BlazorApp3/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace BlazorApp3
+{
+    public static class Startup
+    {
+        public static WebAssemblyHostBuilder InitializeApp(String[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            Configure(builder);
+            ConfigureServices(builder);
+            return builder;
+        }
+        private static void ConfigureServices(WebAssemblyHostBuilder builder)
+        {
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        }
+
+        private static void Configure(WebAssemblyHostBuilder builder)
+        {
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+        }
+    }
+}
